5852f4b33bd63a10c98635914d43b912e18a72f7
muzakkir2045 <muzakkir2045@gmail.com>
2026-03-26 21:54:02 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,7 +5,10 @@ def main():
     while True:
         sys.stdout.write("$ ")
         command = input()
-        print(f"{command}: command not found ")
+        if command == "exit":
+            break
+        else:
+            print(f"{command}: command not found ")
     pass
 
 
