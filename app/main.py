a74541c0bcc2014de4c4592dfd3e8e6140c32e2b
muzakkir2045 <muzakkir2045@gmail.com>
2026-03-26 22:07:38 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -14,7 +14,6 @@ def main():
             continue
         elif command.startswith("type ") and command[5:] in builtins:
             print(f"{command[5:]} is a shell builtin")
-            continue
         else:
             print(f"{command}: command not found ")
 
