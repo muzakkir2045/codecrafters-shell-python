acaa601cf61ebfe61cc7a32141b90e2cdd4d1c81
muzakkir2045 <muzakkir2045@gmail.com>
2026-03-26 21:46:44 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -2,8 +2,8 @@ import sys
 
 
 def main():
-    
-    command = input("$ ")
+    sys.stdout.write("$ ")
+    command = input()
     if command == "xyz":
         sys.stdout.write(f"{command}: command not found ")
     pass
